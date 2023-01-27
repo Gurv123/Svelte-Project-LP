46eee6de90a9b0c19be06778c2be19e3c7769ca9
Gurvan B <gurvan.buanic123@gmail.com>
2023-01-27 03:42:58 +0100

    Reset project
    Co-authored-by: Volpe08 <Volpe0008@users.noreply.github.com>
    Co-authored-by: Florian Chicot <florian-chicot@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,11 @@
 <script>
   import NavBar from "../components/NavBar.svelte";
   import Footer from "../components/Footer.svelte";
-  import Main from "../components/Main.svelte";
+  import Home from "../components/Home.svelte";
 </script>
 
 <div class="wrapperMain">
   <NavBar />
-  <Main />
+  <Home />
   <Footer />
 </div>
-
-<style>
-  .wrapperMain {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-</style>
